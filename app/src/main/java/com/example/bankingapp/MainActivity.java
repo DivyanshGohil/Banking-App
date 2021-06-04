3f8f06c7f671c93636f2624a9d829fc4ffb21e3d
Divyansh Gohil <divyansh.gohil@gmail.com>
2021-06-04 13:10:18 +0530

    First Commit

---
+++ app/src/main/java/com/example/bankingapp/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.bankingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN = 5000;
+
+    //variables
+    Animation topAnim, bottomAnim;
+    ImageView bankImage;
+    TextView name,by;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+
+        //Animation
+        topAnim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+
+        bankImage = (ImageView) findViewById(R.id.banklogo);
+        name      = (TextView) findViewById(R.id.name);
+        by        = (TextView) findViewById(R.id.createdBy);
+
+        bankImage.setAnimation(topAnim);
+        name.setAnimation(bottomAnim);
+        by.setAnimation(bottomAnim);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this,HomeScreen.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_SCREEN);
+    }
+}
