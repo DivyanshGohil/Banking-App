70036654fd8bb9961e03bd010b16550d6f2919ff
Divyansh Gohil <divyansh.gohil@gmail.com>
2021-06-08 13:41:44 +0530

    SQLite database Created and dummy value added

--- app/src/main/java/com/example/bankingapp/HomeScreen.java
+++ app/src/main/java/com/example/bankingapp/HomeScreen.java
@@ -16,6 +16,8 @@ public class HomeScreen extends AppCompatActivity {
     Animation blink_anim;
     Button userbtn;
 
+    DatabaseHelper myDb;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,6 +33,10 @@ public class HomeScreen extends AppCompatActivity {
         text1.startAnimation(blink_anim);
         text2.startAnimation(blink_anim);
 
+        myDb  = new DatabaseHelper(this);
+        //Insert values
+        AddDummy();
+
         userbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -38,5 +44,19 @@ public class HomeScreen extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    public void AddDummy(){
+        //inserting 10 dummy data
+        myDb.insertData("1111111111","Aakash patel","xyz@gmail.com","21","0264211111","10000");
+        myDb.insertData("2222222222","Lakshansh padhiyar","xyz@gmail.com","30","0264222222","25000");
+        myDb.insertData("3333333333","Bhargav gohil","xyz@gmail.com","25","0264233333","15000");
+        myDb.insertData("4444444444","vikas sarvaiya","xyz@gmail.com","45","0264244444","45000");
+        myDb.insertData("5555555555","Piyush patel","xyz@gmail.com","38","0264255555","30000");
+        myDb.insertData("6666666666","vivek modi","xyz@gmail.com","41","0264266666","60000");
+        myDb.insertData("7777777777","selmon bhai","xyz@gmail.com","20","0264277777","5000");
+        myDb.insertData("8888888888","Ruchit yadav","xyz@gmail.com","19","0264288888","3500");
+        myDb.insertData("9999999999","Sagar prajapati","xyz@gmail.com","55","0264299999","65000");
+        myDb.insertData("1234567890","Carry minati","xyz@gmail.com","75","0264200000","150000");
     }
 }
--- app/src/main/java/com/example/bankingapp/users.java
+++ app/src/main/java/com/example/bankingapp/users.java
@@ -1,14 +1,26 @@
 package com.example.bankingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.database.Cursor;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class users extends AppCompatActivity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_users);
+
+
     }
+
+
 }
