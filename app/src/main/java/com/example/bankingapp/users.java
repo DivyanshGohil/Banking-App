dd55c1d9a7370e5a5118745fd2f0ee85774dda81
Divyansh Gohil <divyansh.gohil@gmail.com>
2021-06-09 13:14:04 +0530

    name is fetched from DB and view in recycleview

--- app/src/main/java/com/example/bankingapp/users.java
+++ app/src/main/java/com/example/bankingapp/users.java
@@ -6,21 +6,45 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.database.Cursor;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class users extends AppCompatActivity {
 
+    RecyclerView recyclerView;
+    DatabaseHelper myDB;
+    ArrayList<String> user_name;
 
-
-
+    customAdapter customAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_users);
+        recyclerView = (RecyclerView) findViewById(R.id.recycleview);
 
+        myDB = new DatabaseHelper(this);
+        user_name = new ArrayList<>();
 
+        storedDatainArray();
+
+        customAdapter = new customAdapter(users.this,user_name);
+        recyclerView.setAdapter(customAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(users.this));
     }
 
+    void storedDatainArray()
+    {
+        Cursor cursor = myDB.readalldata();
+        if(cursor.getCount() == 0){
+            Toast.makeText(this, "No Data", Toast.LENGTH_SHORT).show();
+        }else{
+            while(cursor.moveToNext()){
+                user_name.add(cursor.getString(0));
+            }
+        }
+    }
 
 }
